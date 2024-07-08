9abcc61b6b5d6c5d7ec973d4020a96485d7a7515
aarav <saarav.as19@gmail.com>
2024-07-08 18:20:37 -0400

    Model fixed

--- Data/preprocess.py
+++ Data/preprocess.py
@@ -1,17 +1,14 @@
 import pandas as pd
 
 
-def preprocess(csv_file, api_data):
-    #drivers_df = pd.read_csv('../Data/drivers.csv')
-    df = pd.DataFrame(csv_file)
-    combined_df = pd.merge(api_data, df, on='driver_number', how='left')
+def preprocess_sessions(drivers_csv, session_data):
+    drivers_df = pd.read_csv(drivers_csv)
+    combined_df = pd.merge(session_data, drivers_df, on='session_key', how='left')
     combined_df.fillna(0, inplace=True)
     return combined_df
 
 
-def preprocess_sessions(csv_file, session_data):
-    #drivers_df = pd.read_csv('../Data/drivers.csv')
-    df = pd.DataFrame(csv_file)
-    combined_df = pd.merge(session_data, df, on='session_key', how='left')
+def preprocess(main_df, api_data, suffix='_api'):
+    combined_df = pd.merge(main_df, api_data, on='driver_number', how='left', suffixes=('', suffix))
     combined_df.fillna(0, inplace=True)
     return combined_df
